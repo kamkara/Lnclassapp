259c14f9e9afacde39cc69757e0558f8bbe0bb24
kamkara <lnclass.kamara+ln@gmail.com>
2021-12-23 17:43:32 +0000

    add application config

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+    #Onglet title
+def title
+ base_title = "Lnclass Education"
+  if @title.nil?
+  base_title
+  else
+  "#{@title} | #{base_title}"
+  end
+end
+#site description
+def description
+  "Lnclass Education aide les élèves a apprendre facilement"
+end
 end
